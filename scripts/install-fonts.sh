00f602a79324b3cf72a4599fb5e87ed775ba5b64
comagnaw <comagnaw@gmail.com>
2020-09-25 14:54:52 -0400

    fix typo and add comment

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+printf "# Installing homebrew fonts cask...\n"
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
+printf "# Done installing homebrew fonts cask\n"
--- scripts/install-formulas.sh
+++ scripts/install-formulas.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-printf "# Installing hoembrew formulas...\n"
+printf "# Installing homebrew formulas...\n"
 while read formula; do    
     /usr/local/bin/brew install ${formula}
 done < formulas.txt
