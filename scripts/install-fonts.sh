24b195163e21bea22bd068636b71bb8df57da7da
comagnaw <comagnaw@gmail.com>
2021-05-30 19:37:16 -0400

    fix font install

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -2,5 +2,5 @@
 
 printf "# Installing homebrew fonts cask...\n"
 brew tap homebrew/cask-fonts
-brew cask install font-hack-nerd-font
+brew install cask font-hack-nerd-font
 printf "# Done installing homebrew fonts cask\n"
